1fca325567f131e51533826fa96ae14dee171149
nbidal18 <nizarbidal18@gmail.com>
2024-04-02 16:37:54 +0200

    minor changes, made notes

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 10:53:20 by nbidal            #+#    #+#             */
-/*   Updated: 2024/04/02 12:51:30 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/04/02 16:36:45 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,9 @@ char	*get_next_line(int fd)
 
 }
 
+/*
 int	main(void)
 {
 
 }
-
-//reads from the file BUFFER_SIZE bytes and prints them,
-//then repeats until end of file.
-int	main(void)
-{
-	int		fd;
-	char	buf[BUFFER_SIZE];
-	int		chars_read;
-
-	//fd = open("hello.txt", O_RDONLY);
-	//fd = open("lorem.txt", O_RDONLY);
-	do
-	{
-		chars_read = read(fd, buf, BUFFER_SIZE);
-		buf[chars_read] = '\0';
-		printf("->%s\n", buf);
-	} while (chars_read);
-}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: nbidal <nbidal@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/31 11:39:23 by nbidal            #+#    #+#             */
-/*   Updated: 2024/04/02 12:38:14 by nbidal           ###   ########.fr       */
+/*   Updated: 2024/04/02 16:37:34 by nbidal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,18 @@
 
 typedef struct list
 {
-	char		line[BUFFER_SIZE];
+	char		*str_buf;
 	struct list	*next_line;
 }	t_list;
 char	*get_next_line(int fd);
+void	create_list(); //1
+int		found_new_line(); //2
+void	append(); //2
+t_list	*find_last_node(); //2
+char	*get_line(); //1
+int		len_to_new_line(); //2
+void	copy_str(); //2
+void	polish_list(); //1
+void	dealloc(); //1
 
 #endif
